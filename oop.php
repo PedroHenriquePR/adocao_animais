75c953d12e7ff6291c6ce311e15acb19f213457e
Pedro <h3enr1quepedr011@gmail.com>
2025-06-12 10:22:36 -0300

    Includes

---
+++ oop.php
@@ -0,0 +1,8 @@
+<?php
+include_once './Classes/Adotante.class.php';
+include_once './Classes/Animal.class.php';
+include_once './Classes/Cachorro.class.php';
+include_once './Classes/Gato.class.php';
+
+
+?>
